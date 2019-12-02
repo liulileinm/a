7d4d145410ef65d1e2587d9e1e2aa0d974054248
86341 <86341@DESKTOP-QG825HB>
2019-12-02 17:02:02 +0800

    313

--- a/src/main/java/a/AA.java
+++ a/src/main/java/a/AA.java
@@ -1,5 +1,12 @@
 package a;
 
 public class AA {
-
+	public AA() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
 }
